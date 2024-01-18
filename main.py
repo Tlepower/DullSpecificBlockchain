e66feaafa61150dd244e366dcde1798cefd57646
danielakpan140 <31309706-danielakpan140@users.noreply.replit.com>
2024-01-18 18:33:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+file = open("sample.txt","r")
+r = {}
+count = 0
+for line in file:
+  line = line.strip()
+  line = line.replace(".", " ").split()
+  print(line)
+  for word in line:
+    if word in r:
+      r[word] += 1
+    else:
+      r[word] = 1
+file.close()
+print(r["words"])
+
