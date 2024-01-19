a7294235b7b393e0c213669a3ea51688a84facce
Tlepower <139595644+Tlepower@users.noreply.github.com>
2024-01-19 17:32:42 +0000

    This is the real code

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ count = 0
 for line in file:
   line = line.strip()
   line = line.replace(".", " ").split()
-  print(line)
   for word in line:
     if word in r:
       r[word] += 1
     else:
       r[word] = 1
 file.close()
-print(r.keys())
-print(r.values())
+print(max(r.values()))
 
 
